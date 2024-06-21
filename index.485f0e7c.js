65775c54d987a53e29a37bb7ebb66fdc7b7b162f
Voloshynd <Voloshynd13@gmail.com>
2024-06-21 23:20:18 +0200

    make build

---
+++ index.485f0e7c.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.top="".concat(50,"%"),t.style.left="".concat(50,"%"),t.style.transform="translate(".concat(-50,"%, ",-50,"%)");
+//# sourceMappingURL=index.485f0e7c.js.map
